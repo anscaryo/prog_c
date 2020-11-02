0d56d946c491e6456dce29fe7dc3d46ea6f92a19
anscaryo <anscaryo@gmail.com>
2020-11-02 08:42:03 +0100

    primera practica FP_20_21

---
+++ Practica01/Practica01.cpp
@@ -0,0 +1,18 @@
+/***************************************
+* NOMBRE: #O
+scar#
+* PRIMER APELLIDO: #Flores#
+* SEGUNDO APELLIDO: #Hermoso#
+* DNI: #28896828L#
+* EMAIL: #oflores@alumnos.uned.es#
+* **************************************/
+#include <stdio.h>
+
+int main(){
+    printf( "Oscar\n" );
+    printf( "Flores\n" );
+    printf( "Hermoso\n" );
+    printf( "28896828L\n" );
+    printf( "oflores@alumnos.uned.es\n" );
+    return 0;
+}
---
+++ Practica01/practica_2.cpp
@@ -0,0 +1,7 @@
+/***************************************
+* NOMBRE: #Oscar#
+* PRIMER APELLIDO: #Flores#
+* SEGUNDO APELLIDO: #Hermoso#
+* DNI: #28896828L#
+* EMAIL: #oflores@alumnos.uned.es#
+* **************************************/
